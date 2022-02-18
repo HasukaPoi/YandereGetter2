001677880ce812cb4182a96e3bc3f43f5a781d24
hachan <hasukapyon@163.com>
2022-02-18 19:49:51 +0800

    first commit

---
+++ src/components/todolist.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            //初期化state，目前还没有东西
+        }
+    }
+}
