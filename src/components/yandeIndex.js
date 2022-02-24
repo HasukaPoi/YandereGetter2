1ef2191cefa96df52f5c4334c76b8c4e2aa5f82a
hachan <hasukapyon@163.com>
2022-02-24 20:35:33 +0800

    add functions

--- src/components/yandeIndex.js
+++ src/components/yandeIndex.js
@@ -7,6 +7,9 @@ class YandeIndex extends React.Component {
     apiUrl2 = 'https://yande.re/post.json';
     attrToShow = ["id", "tags", "source", "width", "height", "file_ext", "file_size", "preview_url", "sample_url", "jpeg_url", "file_url"];
 
+    //与this.state.keyword 的区别是
+    //这个变量里添加了非NSFW模式用的参数rating:s
+    //并且不会反映到搜索框中
     keyword = "";
 
     constructor(props) {
@@ -14,41 +17,76 @@ class YandeIndex extends React.Component {
 
         //关键字输入框
         this.inputKeyword = React.createRef();
+        //NSFW模式开关
+        this.checkNsfw = React.createRef();
 
-        this.search = this.search.bind(this);
+        this.inputLimit = React.createRef();
+
+        //binding
+        this.handleKeyword = this.handleKeyword.bind(this);
+        this.refreshPosts = this.refreshPosts.bind(this);
+        this.changeSetting = this.changeSetting.bind(this);
 
         this.state = {
-            //初期化state，目前还没有东西
             posts: [],
             keyword: "",
             loading: false,
+            nsfw: false,
+            limit: 40,
+            page: 1,
+            settingChanged: false
         }
     }
 
     //执行搜索
-    search(event) {
+    handleKeyword(event) {
+        this.setState({
+            page: 1,
+            keyword: this.inputKeyword.current.value,
+        })
         if (event.keyCode === 13) {
-            // this.setState({
-            //     keyword: this.inputKeyword.current.value,
-            // });
             this.keyword = this.inputKeyword.current.value;
             this.refreshPosts();
         }
     }
 
+    changeSetting() {
+        this.setState({
+            nsfw: this.checkNsfw.current.checked,
+            limit: this.inputLimit.current.value,
+            settingChanged: true,
+        });
+    }
+
+    changePage(num) {
+        if (num < 1) { return; }
+        this.setState({
+            page: num
+        })
+        this.refreshPosts();
+    }
+
     makeApiUrl() {
         let para = "";
-        if (this.keyword.trim() !== "") {
-            para = para + "?tags=" + encodeURI(this.keyword.trim());
-            console.log(para);
+        let keyword = this.keyword;
+        keyword = keyword.trim();
+        if (!keyword.includes("rating") && !this.state.nsfw) {
+            keyword = "rating:s " + keyword
         }
+        console.log(keyword);
+        para = para + "?tags=" + encodeURI(keyword);
+        para = para + "&limit=" + this.state.limit;
+        para = para + "&page=" + this.state.page;
         this.apiUrl2 = this.apiUrl + para;
         console.log(this.apiUrl2);
     }
 
     refreshPosts() {
         this.makeApiUrl();
-        this.setState({ loading: true });
+        this.setState({
+            loading: true,
+            settingChanged: false,
+        });
         axios({ method: 'get', url: `${this.apiUrl2}` })
             .then(response => {
                 console.log(response.data)
@@ -88,7 +126,8 @@ class YandeIndex extends React.Component {
                 style={{ maxWidth: "150px", maxHeight: "150px" }} /></a></div>
             <div>ID: {post.id}</div>
             <div>Tags: {post.tags}</div>
-            <div>{post.file_ext} {post.width}*{post.height}</div>
+            <div>Rating: {post.rating}</div>
+            <div>File Size: {post.file_ext} {post.width}*{post.height}</div>
             <div>Source: <a href={post.source}>{post.source}</a></div>
             <div><a href={post.sample_url}>Download Sample({this.calcFileSize(post.sample_file_size)} {post.sample_width}*{post.sample_height})</a></div>
         </div>;
@@ -97,7 +136,19 @@ class YandeIndex extends React.Component {
 
     makeMenus() {
         return <div style={{ margin: "1em", padding: "1em", backgroundColor: "lightpink" }}>
-            <div>Keyword: <input type="text" ref={this.inputKeyword} onKeyUp={this.search}></input></div>
+            <div>关键字(以空格分隔): <input type="text" value={this.state.keyword} ref={this.inputKeyword} onChange={this.handleKeyword} onKeyUp={this.handleKeyword}></input><button onClick={this.refreshPosts}>搜索</button></div>
+            <div>设置{this.state.settingChanged ? "【未生效】" : ""}　※修改后需要重新执行搜索才有效。关键字中指定了rating的情况下覆盖NSFW模式设置项。</div>
+            <div>NSFW模式: <input type="checkbox" checked={this.state.nsfw} ref={this.checkNsfw} onChange={this.changeSetting}></input></div>
+            <div>每页post数量：<input type="text" value={this.state.limit} ref={this.inputLimit} onChange={this.changeSetting}></input></div>
+        </div>;
+    }
+
+    makePageTurner() {
+        return <div>
+            <input type="button" value="上一页" disabled={this.state.page === 1} onClick={this.changePage.bind(this, this.state.page - 1)} />
+                当前在第 {this.state.page} 页（第{(this.state.page - 1) * this.state.limit + 1}～{this.state.page * this.state.limit}张）
+            <input type="button" value="下一页" onClick={this.changePage.bind(this, this.state.page + 1)} />
+            跳转：<input type="text" value={this.state.page} />
         </div>;
     }
 
@@ -105,20 +156,30 @@ class YandeIndex extends React.Component {
         this.refreshPosts();
     }
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        // if (prevState.page!==this.state.page){
+        //     this.refreshPosts();
+        // }
+    }
+
     render() {
-        if (this.state.loading) {
-            return <div>Loading</div>;
-        } else {
-            return (<div>
-                <h1>yande.re getter 2</h1>
-                {this.makeMenus()}
-                <div>{
-                    this.state.posts.map((post, index) => {
-                        return this.makeOnePost(post);
-                    })
-                }</div>
-            </div>);
+        let mainpart = <div>Loading...</div>
+        if (!this.state.loading) {
+            mainpart = <div>{
+                this.state.posts.map((post, index) => {
+                    return this.makeOnePost(post);
+                })
+            }</div>
         }
+
+        return (<div>
+            <h1>yande.re getter 2</h1>
+            {this.makeMenus()}
+            {this.makePageTurner()}
+            {mainpart}
+            {this.makePageTurner()}
+        </div>);
+
     }
 }
 
